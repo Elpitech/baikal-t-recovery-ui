591b3c1ab8a20f01441755158fcdf7bc7e91f637
Konstantin Kirik <smlcx.mail@gmail.com>
2017-02-03 12:58:20 +0300

    Added log flushing after each message

--- src/common.h
+++ src/common.h
@@ -2,8 +2,8 @@
 #define __COMMON_H__
 
 extern FILE *logf;
-#define log(...) fprintf (logf, "L["TAG"]: "__VA_ARGS__)
-#define warn(...) fprintf (logf, "W["TAG"]: "__VA_ARGS__)
-#define err(...) fprintf (logf, "E["TAG"]: "__VA_ARGS__)
+#define log(...) {fprintf (logf, "L["TAG"]: "__VA_ARGS__); fflush(logf); }
+#define warn(...) {fprintf (logf, "W["TAG"]: "__VA_ARGS__); fflush(logf); }
+#define err(...) {fprintf (logf, "E["TAG"]: "__VA_ARGS__); fflush(logf); }
 
 #endif/*__COMMON_H__*/
