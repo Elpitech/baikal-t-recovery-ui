c2de0000b50c29233ef0fb190efae7a42b4e8ad1
Konstantin Kirik (snegovick) <snegovick@uprojects.org>
2018-02-15 04:58:09 +0300

    Replaced wget with curl

--- getrom.sh
+++ getrom.sh
@@ -4,11 +4,10 @@ VAR_PREFIX=/var/run/recoveryui/
 URL=$1
 
 cd ${VAR_PREFIX}
-TIMEOUT=30
 echo "I: Fetching ${URL}"
 if [ -e /tmp/update-web.rom ]; then rm /tmp/update-web.rom; fi
-echo "${VAR_PREFIX}: /usr/bin/wget -T ${TIMEOUT} "${URL}" -O /tmp/update-web.rom"
-/usr/bin/wget -T ${TIMEOUT} "${URL}" -O /tmp/update-web.rom
+echo "${VAR_PREFIX}: /usr/bin/curl "${URL}" -o /tmp/update-web.rom"
+/usr/bin/curl "${URL}" -o /tmp/update-web.rom
 RET=$?
 if [ ${RET} -ne 0 ]; then
     if [ -e /tmp/update-web.rom ]; then rm /tmp/update-web.rom; fi
